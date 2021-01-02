a0ace0dd84fecbf00ba29adf8435aef9a093dcc7
AlbertHZL <59431832+AlbertHZL@users.noreply.github.com>
2021-01-02 13:53:04 +0800

    Update and rename Source codes/Matplotlib3DWidget.py to src/Matplotlib3DWidget.py

--- src/Matplotlib3DWidget.py
+++ src/Matplotlib3DWidget.py
@@ -1,34 +1,26 @@
 import matplotlib
 import numpy as np
-matplotlib.use("Qt5Agg")#声明使用QT5
+matplotlib.use("Qt5Agg")
 from PyQt5.QtWidgets import *
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 from matplotlib.figure import Figure
-from mpl_toolkits.mplot3d import Axes3D #axes3d
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import *
 
 class MyMplCanvas(FigureCanvas):
     def __init__(self, parent=None):
-        # 配置中文显示
-        #plt.rcParams['font.family'] = ['SimHei']  # 用来正常显示中文标签
-        plt.rcParams['font.family'] = ['Times New Roman']  # 用来正常显示中文标签
-        plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号        
-        plt.rcParams['savefig.dpi'] = 300 #图片像素
-        #plt.rcParams['savefig.metadata'] = 'Creator'  #图片像素
-        #plt.rcParams['figure.dpi'] = 150 #分辨率
         
-        self.fig = Figure()  # 新建一个figure
-        #self.ax =self.fig.add_subplot(111,projection='3d')
+        plt.rcParams['font.family'] = ['Times New Roman']
+        plt.rcParams['axes.unicode_minus'] = False    
+        plt.rcParams['savefig.dpi'] = 300
+       
+        self.fig = Figure() 
         self.ax = Axes3D(self.fig)
-        #self.axes = self.fig.add_subplot(111)  # 建立一个子图，如果要建立复合图，可以在这里修改
         FigureCanvas.__init__(self, self.fig)
         self.setParent(parent)
-        '''定义FigureCanvas的尺寸策略，这部分的意思是设置FigureCanvas，使之尽可能的向外填充空间。'''
-        FigureCanvas.setSizePolicy(self,
-                                   QSizePolicy.Expanding,
-                                   QSizePolicy.Expanding)
+        FigureCanvas.setSizePolicy(self, QSizePolicy.Expanding, QSizePolicy.Expanding)
         FigureCanvas.updateGeometry(self)
 
     def D3Paint(self, fileName, x, y, z, xunit, yunit, zunit, colorbarTitle):
@@ -39,24 +31,18 @@ class MyMplCanvas(FigureCanvas):
         ellipses = []
         for num in range(0,times):
             ellipses.append(z[lengthX*num:lengthX*(num+1)])
-        xx = np.linspace(min(x),max(x),lengthX)     #根据数据特点分为100等份
+        xx = np.linspace(min(x),max(x),lengthX)
         yy = np.linspace(min(y),max(y),lengthX)
-        X,Y = np.meshgrid(xx,yy)      #将x，y坐标格网化
-        #Z = pd.DataFrame(z.reshape(lengthX,lengthX))     #将插值数据转化为矩阵形式
+        X,Y = np.meshgrid(xx,yy)
         xunitnew = 'x/'+xunit
         yunitnew = 'y/'+yunit
         zunitnew = 'v/'+zunit
         self.ax.set_xlabel(xunitnew)
         self.ax.set_ylabel(yunitnew)
         self.ax.set_zlabel(zunitnew)
-        #ax.set_zlim3d(-40,40)
-        #plt.xticks(rotation= 50)  # 改变三维坐标轴的方向
-        #plt.yticks(rotation= -50)
         
-        #self.ax.contourf(X,Y,ellipses,cmap=cm.jet, linewidth=0.1)#1
         ellipses = np.array(ellipses)
         surf = self.ax.plot_surface(X, Y, ellipses, rstride=1, cstride=1,cmap=cm.jet)
-        # 添加映射到图像中的颜色条
         cbar = self.fig.colorbar(surf, extend='neither', spacing='proportional',
                         orientation='vertical', shrink=0.85, format="%.0f")
         cbar.set_label(colorbarTitle, size=10)
@@ -72,6 +58,6 @@ class Matplotlib3DWidget(QWidget):
     def initUi(self):
         self.layout = QVBoxLayout(self)
         self.mpl = MyMplCanvas(self)
-        self.mpl_ntb = NavigationToolbar(self.mpl, self)  # 添加完整的 toolbar
+        self.mpl_ntb = NavigationToolbar(self.mpl, self)
         self.layout.addWidget(self.mpl)
         self.layout.addWidget(self.mpl_ntb)
