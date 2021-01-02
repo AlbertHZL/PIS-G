456939e0b1e86787f5f9a54deaf6d1dc7b525865
AlbertHZL <59431832+AlbertHZL@users.noreply.github.com>
2021-01-02 14:27:11 +0800

    Update and rename Source codes/grav_rfi_ompcuda.h to src/grav_rfi_ompcuda.h

--- src/grav_rfi_ompcuda.h
+++ src/grav_rfi_ompcuda.h
@@ -1,4 +1,4 @@
-﻿#pragma once
+#pragma once
 
 extern "C"  __declspec(dllexport) double* foo(int h_point_count, int h_prism_count, int h_lx, int h_ly, int h_lz, int h_kmax, int a, int b, double h_z_obs, double h_dz, double h_zmax,
 	double h_m_min, double h_m_max, double h_epsilon, double h_lambda, double h_sigma,double wn, double* zc, double* thick, double* Vz, double* x, double* y);
