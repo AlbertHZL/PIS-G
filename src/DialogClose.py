7b76032ed4352a3440307c2adb7a6d4937f0ef3f
AlbertHZL <59431832+AlbertHZL@users.noreply.github.com>
2021-01-02 12:12:04 +0800

    Update and rename Source codes/DialogClose.py to src/DialogClose.py

--- src/DialogClose.py
+++ src/DialogClose.py
@@ -11,11 +11,10 @@ class DialogClose(QDialog):
         layout = QVBoxLayout()
         layout.addStretch(1)
         label = QLabel(self)
-        label.setText("Do you want to close the current project？")#是否关闭当前项目
+        label.setText("Do you want to close the current project？")
         label.setAlignment(Qt.AlignCenter)
         layout.addWidget(label, 1)
         
-        #添加按钮
         layoutBottom = QHBoxLayout()
         layoutBottom.addStretch(5)
         ok = QPushButton("OK", self)
@@ -29,21 +28,21 @@ class DialogClose(QDialog):
         
         self.setLayout(layout)
         self.setWindowTitle("Tips")
-        #去掉问号
+        
         self.setWindowFlags(Qt.Dialog | Qt.WindowCloseButtonHint)
-        #信号槽
+        
         ok.clicked.connect(self.on_ok_clicked)
         cancel.clicked.connect(self.on_cancel_clicked)
         
     def on_ok_clicked(self):
-        #获取索引
+        
         position = self.father.tab.currentIndex()
-        #更改tree_record项目
+        
         file_name = str(self.father.tree.topLevelItem(position-1).text(0))
         del self.father.tree_record[file_name]
-        #删除tab页
+        
         self.father.tab.removeTab(position)
-        #删除文件树
+        
         self.father.tree.takeTopLevelItem(position-1)
         self.close()
         return
