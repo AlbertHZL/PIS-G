88bf49483a80094a71c933316d74328eb128ae57
AlbertHZL <59431832+AlbertHZL@users.noreply.github.com>
2021-01-02 14:18:39 +0800

    Update and rename Source codes/TreeWidget.py to src/TreeWidget.py

--- src/TreeWidget.py
+++ src/TreeWidget.py
@@ -7,24 +7,22 @@ class TreeWidget(QTreeWidget):
         super(TreeWidget, self).__init__(parent)
         self.father = parent
         
-        #右键事件
         self.rightKeyMenu = QMenu()
         self.actionSave = QAction("Save", self)
         self.actionDelete = QAction("Delete", self)
         self.actionExpand = QAction("Spread", self)
         self.actionZhe = QAction("Fold", self)
-        #正演数据的右键事件
+        
         self.actionPaint = QAction("Painting",  self)
         self.actionForwardingSave = QAction("Save Data", self)
         
-        #信号槽
         self.actionSave.triggered.connect(self.on_Save)
         self.actionDelete.triggered.connect(self.on_Delete)
         self.actionExpand.triggered.connect(self.on_Expand)
         self.actionZhe.triggered.connect(self.on_Zhe)
         self.actionPaint.triggered.connect(self.father.paintModel)
         self.actionForwardingSave.triggered.connect(self.father.forwardingSave)
-    #保存操作
+    
     def on_Save(self):
         selectedList = self.father.tree.selectedItems()
         if len(selectedList)!=1:
@@ -37,8 +35,7 @@ class TreeWidget(QTreeWidget):
         name = QFileDialog.getSaveFileName(self, 'Open File', './', '*.png *.pdf *.jpg')
         mw.mpl.saveFig(name[0])
         self.father.status.showMessage("Picture Saves Successfully", 3000)
-        
-    #删除操作
+    
     def on_Delete(self):
         selectedList = self.father.tree.selectedItems()
         if len(selectedList)!=1:
@@ -46,7 +43,7 @@ class TreeWidget(QTreeWidget):
         item = selectedList[0]
         root = item.parent()
         ind = root.indexOfChild(item)
-        #更改tree_record
+        
         root_name = root.text(0)
         order = 0
         for key,  value in self.father.tree_record[root_name].items():
@@ -57,27 +54,24 @@ class TreeWidget(QTreeWidget):
         root.takeChild(ind)
         mdiarea=self.father.tab.currentWidget()
         mdiarea.removeSubWindow(mdiarea.subWindowList()[ind])
-        
-    #展开操作
+   
     def on_Expand(self):
         selectedList = self.father.tree.selectedItems()
         if len(selectedList)!=1:
             return
         item = selectedList[0]
         item.setExpanded(1)
-        
-    #折叠操作
+   
     def on_Zhe(self):
         selectedList = self.father.tree.selectedItems()
         if len(selectedList)!=1:
             return
         item = selectedList[0]
         item.setExpanded(0)
-        
-    #右键事件
+   
     def contextMenuEvent(self, event):
         self.rightKeyMenu.clear()
-        #得到窗口坐标
+        
         point = event.pos()
         item = self.itemAt(point)
         if item==None:
@@ -90,9 +84,6 @@ class TreeWidget(QTreeWidget):
                 else:
                     self.rightKeyMenu.addAction(self.actionZhe)
             if self.father.tree.topLevelItem(i)==item.parent():
-                #如果是第一个孩子，则没有右键目录
-                #如果是file data，则没有右键目录
-                #如果是正演产生的数据，则加上 painting 和 save data
                 root = item.parent().text(0)
                 child = item.text(0)
                 if child not in self.father.tree_record[root]:
@@ -108,6 +99,6 @@ class TreeWidget(QTreeWidget):
                 else:
                     self.rightKeyMenu.addAction(self.actionSave)
                     self.rightKeyMenu.addAction(self.actionDelete)
-        #菜单出现的位置为当前鼠标的位置
+       
         self.rightKeyMenu.exec(QCursor.pos())
         event.accept()
