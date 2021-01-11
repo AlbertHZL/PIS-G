453ccd67e65ba2c1f7a84bde878359dd02bc9c4b
AlbertHZL <59431832+AlbertHZL@users.noreply.github.com>
2021-01-11 22:11:59 +0800

    Update grav_rfi_ompcuda.h

--- src/grav_rfi_ompcuda.h
+++ src/grav_rfi_ompcuda.h
@@ -7,8 +7,8 @@
 #endif
 
 extern "C" {
-	double* DLLEXPORT foo(int h_point_count, int h_prism_count, int h_lx, int h_ly, int h_lz, int h_kmax, int a, int b, double h_z_obs, double h_dz, double h_zmax,
+	DLLEXPORT double* foo(int h_point_count, int h_prism_count, int h_lx, int h_ly, int h_lz, int h_kmax, int a, int b, double h_z_obs, double h_dz, double h_zmax,
 	double h_m_min, double h_m_max, double h_epsilon, double h_lambda, double h_sigma,double wn, double* zc, double* thick, double* Vz, double* x, double* y);
 
-	int DLLEXPORT CheckCount();
+	DLLEXPORT int CheckCount();
 }
