b620c9aea42848ba8c311bcb410eaa7279ca6cf5
AlbertHZL <59431832+AlbertHZL@users.noreply.github.com>
2021-01-11 22:06:55 +0800

    Update Forwarding_DLL.h

--- src/Forwarding_DLL.h
+++ src/Forwarding_DLL.h
@@ -7,6 +7,6 @@
 #endif
 
 extern "C" {
-	double* DLLEXPORT Forwarding(int lx, int ly, int point_count, int prism_count, double x_min, double dx, double x_max,
+	DLLEXPORT double* Forwarding(int lx, int ly, int point_count, int prism_count, double x_min, double dx, double x_max,
 	double y_min, double dy, double y_max, double z_obs, double *x, double *y, double *mx, double *my, double *mz, double *p);
 }
